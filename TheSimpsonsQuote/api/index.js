4edae156fe6df2cff578762d623c6a98b7ebb9ac
Lisandro Ramirez Maisuls <ramirezmlisandro@gmail.com>
2022-11-29 11:11:03 -0300

    firsts attempt on DB and server

--- TheSimpsonsQuote/api/index.js
+++ TheSimpsonsQuote/api/index.js
@@ -1,6 +1,10 @@
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
 
-
-sequelize.sync().then(() => {
+// server.listen(3001, () => {
+//   console.log('Listening on port 3001')
+// })
+conn.sync({force: true}).then(() => {
   server.listen(3001, () => {
     console.log('listening on port 3001')
   })
---
+++ TheSimpsonsQuote/api/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const server = express();
+
+server.use(morgan('dev'));
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+module.exports = server
+
